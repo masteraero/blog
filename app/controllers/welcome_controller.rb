e7d66a74a7562685541dd9a7d4225f41a6756ea4
Aaron None <masteraero@gmail.com>
2014-04-30 11:04:50 -0400

    a

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,18 @@
 class WelcomeController < ApplicationController
   def index
+    @counter = increment_counter
+    @shown_message = "You've been here #{@count} times"
   	if session['visited']
   		@first_visit = false
   	else
   		@first_visit = true
   	end
   	session['visited'] = true
+  end
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
   end
 end
