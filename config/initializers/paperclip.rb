247d1d0d7265f2e6a42c4dfb376cb227824939a8
Aaron None <masteraero@gmail.com>
2014-05-20 10:44:36 -0400

    slight tweaks to aws

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 require "paperclip"  
 Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.8.9-Q16' 
 Paperclip.options[:swallow_stderr] = false
-Paperclip::Attachment.default_options[:url] = ':blog1234567.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'blog1234567.s3-website-us-east-1.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
